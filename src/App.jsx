bebd1074083d142e29a7a0b2879e84d540cfbbc7
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-08 17:55:18 +0200

    creado CategoryNav

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.scss";
 import HeaderDesk from "./components/HeaderDesk";
 import HeaderMobile from "./components/HeaderMobile";
 import MediaQuery from "react-responsive";
+import CategoryNav from "./components/CategoryNav";
 
 function App() {
   return (
     <div className="app">
       <MediaQuery minWidth={600}>
         <HeaderDesk />
+        <CategoryNav />
       </MediaQuery>
       <MediaQuery maxWidth={600}>
         <HeaderMobile />

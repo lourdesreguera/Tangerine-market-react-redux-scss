f55845c8af02fab3ee679417fdf4f8416f5ddbfb
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-08 13:54:30 +0200

    añado react-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.scss';
-import HeaderDesk from './components/HeaderDesk';
-import HeaderMobile from './components/HeaderMobile';
+import "./App.scss";
+import HeaderDesk from "./components/HeaderDesk";
+import HeaderMobile from "./components/HeaderMobile";
+import MediaQuery from "react-responsive";
 
 function App() {
   return (
     <div className="app">
-      {/* <HeaderMobile /> */}
-      <HeaderDesk />
+      <MediaQuery minWidth={600}>
+        <HeaderDesk />
+      </MediaQuery>
+      <MediaQuery maxWidth={600}>
+        <HeaderMobile />
+      </MediaQuery>
     </div>
   );
 }

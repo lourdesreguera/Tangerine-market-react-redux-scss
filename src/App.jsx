7d51115a93761f174f72b3e341dff85f7cedf470
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-09 17:03:47 +0200

    reordenacion rutas

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import MyAccountPage from "./pages/MyAccountPage";
 
-
 function App() {
   return (
     <div className="app">
@@ -22,17 +21,12 @@ function App() {
       <MediaQuery maxWidth={600}>
         <HeaderMobile />
       </MediaQuery>
-
-      {/* <RegisterPage /> */}
-      <LoginPage />
-      <Footer />
-
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/my-account" element={<MyAccountPage />} />
       </Routes>
-
+      <Footer />
     </div>
   );
 }

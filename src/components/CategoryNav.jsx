0c2deda2ebc60950138f5b0cba63b5d4b79a1d4d
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-10 14:11:53 +0200

    creado categories

--- src/components/CategoryNav.jsx
+++ src/components/CategoryNav.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
-const CategoryNav = () => {
+const CategoryNav = ({handleCategory}) => {
+
+  const showCategory = (ev) => {
+    handleCategory(ev.target.innerHTML)
+  }
+
+  const showAll = () => {
+    handleCategory(null)
+  }
+
   return (
     <nav className="categoryNav">
       <div className="categoryNav__container">
-        <a href="#" className="header__nav-link header__nav-link--categories">
-          Alimentación
-        </a>
-        <a href="#" className="header__nav-link header__nav-link--categories">
-          Floristerías
-        </a>
+      <p className="header__nav-link header__nav-link--categories" onClick={ showAll } >
+          Todo
+        </p>
+        <p className="header__nav-link header__nav-link--categories" onClick={ showCategory }>
+          Alimentacion
+        </p>
+        <p className="header__nav-link header__nav-link--categories" onClick={ showCategory }>
+          Floristeria
+        </p>
       </div>
     </nav>
   );
--- src/pages/StoresPage.jsx
+++ src/pages/StoresPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getStores } from "../redux/stores/stores.actions";
 import CategoryNav from "../components/CategoryNav";
 
@@ -8,17 +8,49 @@ const StoresPage = () => {
   const dispatch = useDispatch();
   const { stores, isLoading, error } = useSelector((state) => state.stores);
 
+  const [category, setCategory] = useState(null);
+
+  const handleCategory = (category) => {
+    setCategory(category);
+  };
+
+  console.log(category);
+
   useEffect(() => {
     dispatch(getStores());
   }, []);
 
+  const filteredStores = stores.filter((store) => store.category === category);
+
   return (
     <>
-      <CategoryNav />
+      <CategoryNav handleCategory={handleCategory} />
       <div className="stores">
         {isLoading && <h2>Cargando...</h2>}
-        {stores &&
+        {category === null &&
           stores.map((store) => {
+            return (
+              <div key={store._id} className="stores__container">
+                <div>
+                  <img src={store.image} />
+                </div>
+                <div>
+                  <a href="#" className="stores__data stores__data--heading">
+                    {store.name}
+                  </a>
+                  <p className="stores__address">{store.address}</p>
+                  <a
+                    href="tel:{store.phone}"
+                    className="stores__data stores__data--phone"
+                  >
+                    {store.phone}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        {filteredStores &&
+          filteredStores.map((store) => {
             return (
               <div key={store._id} className="stores__container">
                 <div>

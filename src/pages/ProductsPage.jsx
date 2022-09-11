b3cef6c829a9f4748185724f5c0d97e48502154d
FJ Tineo <tineox2000@hotmail.com>
2022-09-11 11:57:38 +0200

    ruta del producto al vendedor

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { getProducts } from "../redux/products/products.actions";
-
-
-
-
 
 
 const ProductsPage = () => {
@@ -26,17 +23,25 @@ const ProductsPage = () => {
                 return (
                   <div key={product._id} className="products__container">
                     <div>
-                      <img src={product.photo} alt={product.name}/>
+                      <img width="300px" src={product.photo} alt={product.name}/>
                     </div>
                     <div>
                       <a href="#" className="stores__data stores__data--heading">
-                        {products.name}
+                        {product.name}
                       </a>
                       <p className="stores__address">{product.description}</p>
                       <div>
                         <p>
                             {product.price}
                         </p>
+                      </div>
+                      <div>
+                      <div>
+                      <NavLink to={`/stores/${product.store}`}>
+                      {/* <p>{product.store}</p> */}
+                      <p>Ir a vendedor</p>
+                      </NavLink>
+                      </div>
                       </div>
                     </div>
                   </div>

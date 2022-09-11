4c71186f1c65e03f7fe46005be73ba1f29e91d52
Rafael Villa <rafael.ricardo.villa@gmail.com>
2022-09-11 18:32:57 +0200

    scss products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -17,35 +17,36 @@ const ProductsPage = () => {
     }, []);
 
     return (
-        // <>
-        //   <CategoryNav />
-          <div className="products">
-            {isLoading && <h2>Cargando...</h2>}
-            {products &&
-              products.map((product) => {
-                return (
-                  <div key={product._id} className="products__container">
-                    <div>
-                      <img src={product.photo} alt={product.name}/>
-                    </div>
-                    <div>
-                      <a href="#" className="stores__data stores__data--heading">
-                        {products.name}
-                      </a>
-                      <p className="stores__address">{product.description}</p>
-                      <div>
-                        <p>
-                            {product.price}
-                        </p>
-                      </div>
-                    </div>
+      <>
+        {/* <CategoryNav /> */}
+        <div className="products">
+          {isLoading && <h2>Cargando...</h2>}
+          {products &&
+            products.map((product) => {
+              return (
+                <div key={product._id} className="products__container">
+                  <div className="products__image-container">
+                    <img
+                      className="products__image-img"
+                      src={product.photo}
+                      alt={product.name}
+                    />
                   </div>
-                );
-              })}
-            {error && <h2>{error}</h2>}
-          </div>
-        // </>
-      );
+                  <div className="products__text">
+                    <a href="#" className="stores__data stores__data--heading">
+                      {products.name}
+                    </a>
+                    <h5 className="">{product.description}</h5>
+                    <p>{product.price}</p>
+                  </div>
+                 <button type="submit" className="login-btn login-btn--products">Añadir al Carrito</button>                  
+                </div>
+              );
+            })}
+          {error && <h2>{error}</h2>}
+        </div>
+      </>
+    );
 
 }
 
